c80260126a4c6b2c35ce8eef658f996087945838
gopika <gopikaramanan123@gmail.com>
2023-11-28 05:59:43 +0530

    unwanted consoles

--- TheNewsApp/src/service/getNewsHeadlines.ts
+++ TheNewsApp/src/service/getNewsHeadlines.ts
@@ -3,12 +3,10 @@ import Config from 'react-native-config';
 export async function getCategoryList(category: string) {
   const apiKey = Config.REACT_APP_API_KEY;
 
-  console.log(apiKey);
   try {
     const response = await fetch(
       `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}`,
     );
-    console.log('response', response);
     if (response?.status === 200) {
       const json = await response.json();
       return json;
